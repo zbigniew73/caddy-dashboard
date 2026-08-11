9c6747ab4d9d6d6d3874d4da85e2fe5155bf0346
zbigniew73 <zibi.czechowski@gmail.com>
2026-08-11 05:26:35 +0200

    update 2026-08-11 05:26

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Caddy Dashboard v1.23.3
+# Caddy Dashboard v1.23.4
 
 ![Caddy Dashboard](caddy-dashboard.png)
 
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 set -euo pipefail
 
-APP_VERSION="1.23.3"
+APP_VERSION="1.23.4"
 REPO_URL="${REPO_URL:-https://github.com/zbigniew73/caddy-dashboard.git}"
 BRANCH="${BRANCH:-main}"
 INSTALL_DIR="${INSTALL_DIR:-/opt/caddy-dashboard}"
